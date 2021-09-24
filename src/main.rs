bb4dc119828f0ddfa415307ce4860e0c924de4a7
Badgie <jonaskhansen@gmail.com>
2021-09-24 15:38:42 +0200

    add cli subcmd for interactive benchmark; add about for subcmds

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,20 @@ use std::path::PathBuf;
 use std::process;
 
 #[derive(StructOpt)]
+#[structopt(
+    name = "RAPL.rs",
+    author = "PT10xE21",
+    about = "RAPL measurement tool",
+)]
 enum Cli {
+    #[structopt(about = "Live measurements")]
     Live {
         /// Delay between polls (ms)
         #[structopt(short = "d", long = "delay", default_value = "1000")]
         delay: u64,
     },
+    #[structopt(about = "Measure power consumption of a oneshot script")]
+    // TODO: run n times
     Benchmark {
         /// Benchmark runner application, e.g., python
         #[structopt(parse(from_os_str))]
@@ -21,8 +29,13 @@ enum Cli {
         program: PathBuf,
         /// Args for <program>
         args: Vec<String>
+    },
+    #[structopt(about = "Measure power consumption of an interactive application")]
+    BenchmarkInt {
+        /// Benchmark program
+        #[structopt(parse(from_os_str))]
+        program: PathBuf
     }
-    // TODO: benchmark interactive - e.g., cura
 }
 
 fn main() {
@@ -33,5 +46,8 @@ fn main() {
         Cli::Benchmark { runner, program, args} => {
             tools::benchmark(runner, program, args);
         },
+        Cli::BenchmarkInt { program } => {
+            tools::benchmark_interactive(program);
+        }
     }
 }
