c74949b736e41927ea9b01a10c6ee398803b94df
Badgie <jonaskhansen@gmail.com>
2021-10-01 09:34:52 +0200

    add rapl data model

--- src/models.rs
+++ src/models.rs
@@ -4,3 +4,15 @@ pub(crate) struct RAPLZone {
     pub path: String,
     pub name: String
 }
+
+#[derive(Debug, Clone)]
+pub(crate) struct RAPLData {
+    pub path: String,
+    pub zone: String,
+    pub time_elapsed: f64,
+    pub power_j: f64,
+    pub watts: f64,
+    pub watts_since_last: f64,
+    pub start_power: f64,
+    pub prev_power: f64
+}
