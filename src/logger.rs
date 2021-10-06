4e1136bfaa0f4cdc370f0c353fba65bfc53252da
Badgie <jonaskhansen@gmail.com>
2021-10-06 09:17:11 +0200

    set permissions on csv creation

--- src/logger.rs
+++ src/logger.rs
@@ -1,19 +1,25 @@
 use crate::models;
 
 use csv;
+use std::fs;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::fs::OpenOptions;
 use std::path::Path;
+use std::os::unix::fs::PermissionsExt;
 
 pub(crate) fn log_poll_result(system_start_time: SystemTime, tool: String, zone: models::RAPLData) {
     let file_name = format!("{}-{}.csv", tool, system_start_time.duration_since(UNIX_EPOCH)
         .expect("Failed to check duration").as_secs_f64());
 
     if Path::new(file_name.to_owned().as_str()).exists() {
-        let file = OpenOptions::new().write(true).append(true).open(file_name).unwrap();
+        let file = OpenOptions::new().write(true).append(true).open(file_name.to_owned()).unwrap();
 
         let mut wtr = csv::WriterBuilder::default().has_headers(false).from_writer(file);
         wtr.serialize(zone).expect("Failed to write to file");
+
+        let mut perms = fs::metadata(file_name.to_owned()).unwrap().permissions();
+        perms.set_mode(0o666);
+        fs::set_permissions(file_name.to_string(), perms);
     } else {
         let file = OpenOptions::new().write(true).create(true).open(file_name).unwrap();
 
