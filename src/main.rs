84665ed8a93d4b4726f4ccf9e3b294adaa009679
Jonas Krogh <36476478+Badgie@users.noreply.github.com>
2021-10-06 09:24:08 +0200

    Tool arg for base application (#21)
    * tool as an arg for base application & move delay to base
    
    * update readme

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,22 @@ use std::time::SystemTime;
 #[derive(StructOpt)]
 #[structopt(
     name = "RAPL.rs",
-    author = "PT10xE21",
+    author = "cs-21-pt-9-01",
     about = "RAPL measurement tool",
 )]
-enum Cli {
+struct Cli {
+    /// Delay between polls (ms)
+    #[structopt(short = "d", long = "delay", default_value = "1000")]
+    delay: u64,
+    /// Tool to use
+    #[structopt(subcommand)]
+    tool: Tool
+}
+
+#[derive(StructOpt)]
+enum Tool {
     #[structopt(about = "Live measurements")]
-    Live {
-        /// Delay between polls (ms)
-        #[structopt(short = "d", long = "delay", default_value = "1000")]
-        delay: u64,
-    },
+    Live {},
     #[structopt(about = "Measure power consumption of a oneshot script")]
     Benchmark {
         /// Benchmark runner application, e.g., python
@@ -39,19 +45,13 @@ enum Cli {
         /// Benchmark program
         #[structopt(parse(from_os_str))]
         program: PathBuf,
-        /// Delay between polls (ms)
-        #[structopt(short = "d", long = "delay", default_value = "1000")]
-        delay: u64
     },
     #[structopt(about = "Inline output of a given metric")]
     Inline {
         /// What to measure
         metric: String,
-        /// Delay between polls (ms)
-        #[structopt(short = "d", long = "delay", default_value = "1000")]
-        delay: u64
     },
-    #[structopt(about = "list")]
+    #[structopt(about = "List utility for various RAPL-related information")]
     List {
         /// What to list
         input: String
@@ -60,22 +60,23 @@ enum Cli {
 
 fn main() {
     let system_start_time = SystemTime::now();
-    match Cli::from_args() {
-        Cli::Live { delay } => {
+    let args = Cli::from_args();
+    match args.tool {
+        Tool::Live { } => {
             common::setup_ncurses();
-            tools::live_measurement(delay, system_start_time);
+            tools::live_measurement(args.delay, system_start_time);
         },
-        Cli::Benchmark { runner, program, args, n} => {
+        Tool::Benchmark { runner, program, args, n} => {
             tools::benchmark(runner, program, args, n);
         },
-        Cli::BenchmarkInt { program, delay } => {
+        Tool::BenchmarkInt { program} => {
             common::setup_ncurses();
-            tools::benchmark_interactive(program, delay, system_start_time);
+            tools::benchmark_interactive(program, args.delay, system_start_time);
         },
-        Cli::Inline { metric, delay } => {
-            tools::inline(metric, delay);
+        Tool::Inline { metric} => {
+            tools::inline(metric, args.delay);
         },
-        Cli::List { input } => {
+        Tool::List { input } => {
             tools::list(input);
         }
     }
